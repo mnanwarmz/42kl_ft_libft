2f3d5239d40203f75d3ad05ad39a7c64fa1c858d
Anwar Zaid <mnanwarmz@gmail.com>
2021-06-24 10:14:07 +0800

    strlcpy

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -4,20 +4,20 @@
 size_t	ft_strlcpy(char *dest, const char *src, size_t size)
 {
 	size_t	src_len;
-	size_t	i;
 
 	if (src)
 	{
-		i = 0;
 		src_len = ft_strlen(src);
-		if (size == 0)
-			return (src_len);
-		while (i < size - 1 && (dest[i] || src[i]))
+		if (size > 0)
 		{
-			dest[i] = src[i];
-			i++;
+			if (src_len < size - 1)
+				ft_memcpy(dest, src, src_len + 1);
+			else
+			{
+				ft_memcpy(dest, src, src_len - 1);
+				dest[size - 1] = '\0';
+			}
 		}
-		dest[i] = '\0';
 		return (src_len);
 	}
 	return (0);
