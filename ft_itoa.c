dc6a3076563d34a0ca55793344c1c7ebf210d257
Anwar Zaid <mnanwarmz@gmail.com>
2021-06-29 07:51:50 +0800

    fixed itoa

--- ft_itoa.c
+++ ft_itoa.c
@@ -16,44 +16,36 @@ static char	*ft_strcpy(char *dst, const char *src)
 	return (dst);
 }
 
-static int	ft_intlen(unsigned long n)
+static int	ft_intlen(long n)
 {
 	if (n < 0)
-	{
-		return (ft_intlen(-n) + 2);
-	}
+		return (ft_intlen(-n) + 1);
 	if (n >= 10)
 		return (ft_intlen(n / 10) + 1);
 	return (1);
 }
 
-char	*ft_recursive(int n, char *str)
-{
-	if (n < 0)
-	{
-		str[0] = '-';
-		str[1] = '\0';
-		str = ft_strjoin(str, ft_recursive(-n, str));
-	}
-	else if (n >= 10)
-		str = ft_strjoin(ft_recursive(n / 10, str), ft_recursive(n % 10, str));
-	else if (n < 10 && n >= 0)
-	{
-		str[0] = n + '0';
-		str[1] = '\0';
-	}
-	return (str);
-}
-
 char	*ft_itoa(int n)
 {
 	char	*str;
+	int		len;
 
-	str = (char *)malloc(sizeof(char) * ft_intlen(n) + 1);
+	len = ft_intlen(n) + 1;
+	str = (char *)malloc(sizeof(char) * len);
 	if (!(str))
 		return (NULL);
 	if (n == -2147483648)
 		return (ft_strcpy(str, "-2147483648"));
-	str = ft_recursive(n, str);
+	str[--len] = '\0';
+	if (n < 0)
+	{
+		n *= -1;
+		str[0] = '-';
+	}
+	while (len && n)
+	{
+		str[--len] = (n % 10) + '0';
+		n /= 10;
+	}
 	return (str);
 }
